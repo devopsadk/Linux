97edc1b21e66bd60ae62cb596e9fcf71f07c2ea1
DEVOPS_ENG <devopsadk@gmail.com>
2023-11-03 09:24:20 +0530

    Shell scripts

--- 25_disk_space.sh
+++ 25_disk_space.sh
@@ -14,6 +14,6 @@ Threshold=20
 df -h | awk 'NR>1 {print $5,$6}' | while read -r usage mount_point; do
 usage_percentage=${usage%\%}
 if [ "$usage_percentage" -gt "$Threshold" ]; then
-echo "Mount point "$mount_point" has crossed the threshold value "$Threshold" and its now "usage" "
+echo "Mount point "$mount_point" has crossed the threshold value "$Threshold" and its now "$usage" "
 fi
 done

f446e25106ae37a678462f25a597306185e39e95
DEVOPS_ENG <devopsadk@gmail.com>
2023-11-04 11:25:43 +0530

    Shell Scripts

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-read -p "enter file name along with path" File
 
-if [ -f $File ]
-then
-echo "file exists"
-else
-echo "file dont exist"
-fi
+src_dir=/e/linux_scripts/
+back_dir=/e/backup_dir
+
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+backupfolder="$backup_dir/backup_$TIMESTAMP"
+
+mkdir -p $backupfolder
+
+cp -R $src_dir $backupfolder
